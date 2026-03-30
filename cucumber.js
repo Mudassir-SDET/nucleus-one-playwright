4dc609b6f5f2087d8a14be181db685f1cdcb1748
Mudassir Moavia <Mudassir.SQAE@gmail.com>
2026-03-30 18:03:32 +0500

    Playwright+JS

--- cucumber.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  default: {
-    paths: ['features/**/*.feature'],       // path to your feature files
-    require: ['step-definitions/**/*.js'], // path to your step definitions
-    publishQuiet: true
-  }
-};
--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,17 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-
   testDir: './tests',
   timeout: 60000,
+
+  reporter: [
+    ['list'],
+    ['allure-playwright', { outputFolder: 'allure-results/js', detail: true }] // ✅ important
+  ],
 
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 10000,
   },
-
 });
